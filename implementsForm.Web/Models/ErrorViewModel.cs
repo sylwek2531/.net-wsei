d14974e892329575f83b58ce280a64ac259dfdd2
sylwek2531 <sylwek2531@gmail.com>
2020-10-11 09:35:18 +0200

    Dodaj pliki projektów.

---
+++ implementsForm.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace implementsForm.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
